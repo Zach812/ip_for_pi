0ff30312e185d2d59d9c0fcfe7ea8c1a20e363a2
Zach812 <zach@weinzierl.us>
2024-12-29 13:10:54 -0600

    Adding functrionality with network bridge

--- Demo.py
+++ Demo.py
@@ -4,17 +4,11 @@ import subprocess
 
 # Initialize the LCD with specific parameters: Raspberry Pi revision, I2C address, and backlight status
 lcd = LCD(2, 0x27, True)  # Using Raspberry Pi revision 2 and above, I2C address 0x3f, backlight enabled
-
-result = subprocess.run(["ifconfig"], capture_output=True, text=True)
+lcd.message("Looking for IP")
+result = subprocess.run(["hostname", "-I"], capture_output=True, text=True)
 result = result.stdout.split()
-next_word = False
-ip = None
-for word in result:
-    if word == "inet":
-        next_word = True
-    elif next_word:
-        ip = word
-        break
+ip = result[0]
+
 # Display messages on the LCD
 lcd.message("Current IP", 1)        # Display 'NerdCave!' on line 1
 lcd.message(ip, 2)    # Display '    - Tutoarials' on line 2
